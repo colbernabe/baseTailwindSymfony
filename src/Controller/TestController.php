3b27c75b918358369385cecd064642be9952b9f1
Kenet Ch <kenbok2018@gmail.com>
2023-10-11 01:37:47 -0500

    Updated branch

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -8,11 +8,81 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
 {
-    #[Route('/test', name: 'app_test')]
-    public function index(): Response
+    #[Route('/noticias/{category?}',defaults:["category"=>""], name: 'app_noticias')]
+    public function index($category): Response
     {
+        $noticias = $this->generateNews();
+        $filteredNews = $this->filterNewsByIndex('category',$noticias, $category);
         return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
+            'noticias' => $category!=""?$filteredNews:$noticias,
+            'category' => $category
         ]);
     }
+
+    #[Route('/noticias/detalle/{id?}',defaults:["id"=>""], name: 'app_noticias_detalle')]
+    public function detalle(int $id): Response
+    {
+        $noticias = $this->generateNews();
+        $filterNew = $this->filterNewsByIndex('id',$noticias, $id);
+       
+        return $this->render('test/detail/index.html.twig', [
+            'noticia' => $filterNew,
+        ]);
+    }
+
+    private function generateNews(): Array
+    {
+       return [
+        [
+            'id'=> 1,
+            'category'=>'tecnología',
+            'title'=>'La IA dominará el mundo',
+            'content'=>'Lorem Hola Mundo, primera Noticia,Lorem Hola Mundo, primera Noticia,Lorem Hola Mundo, primera Noticia,Lorem Hola Mundo, primera Noticia',
+        ],
+        [
+            'id'=> 2,
+            'category'=>'tecnología',
+            'title'=>'La IA dominará el mundo2',
+            'content'=>'Lorem Hola Mundo2, primera Noticia,Lorem Hola Mundo, primera Noticia,Lorem Hola Mundo, primera Noticia,Lorem Hola Mundo, primera Noticia',
+        ],
+        [
+            'id'=> 3,
+            'category'=>'tecnología',
+            'title'=>'La IA dominará el mundo3',
+            'content'=>'Lorem Hola Mundo3, primera Noticia,Lorem Hola Mundo, primera Noticia,Lorem Hola Mundo, primera Noticia,Lorem Hola Mundo, primera Noticia',
+        ],
+        [
+            'id'=> 4,
+            'category'=>'deportes',
+            'title'=>'Lo nuevo del futbol',
+            'content'=>'Lorem Hola Mundo, segunda Noticia,Lorem Hola Mundo, segunda Noticia,Lorem Hola Mundo, segunda Noticia,Lorem Hola Mundo, segunda Noticia',
+        ],
+        [
+            'id'=> 5,
+            'category'=>'arte',
+            'title'=>'Lo nuevo del arte',
+            'content'=>'Lorem Hola Mundo, primera Noticia,Lorem Hola Mundo, segunda Noticia,Lorem Hola Mundo, segunda Noticia,Lorem Hola Mundo, segunda Noticia',
+        ],
+        [
+            'id'=> 6,
+            'category'=>'ciencia',
+            'title'=>'Lo nuevo de las ciencas',
+            'content'=>'Lorem Hola Mundo, primera Noticia,Lorem Hola Mundo, segunda Noticia,Lorem Hola Mundo, segunda Noticia,Lorem Hola Mundo, segunda Noticia',
+        ]
+       ];
+    }
+
+    private function filterNewsByIndex(string $index, array $newsArray, string $val) {
+        $filteredNews = [];
+    
+        foreach ($newsArray as $news) {
+            if ($news[$index] == $val) {
+                $filteredNews[] = $news;
+            }
+        }
+    
+        return $filteredNews;
+    }
+
+    
 }
